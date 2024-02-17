e839f773ef19dfd7dafbacb5d61f97d12e43ee32
yukihira-pot <nagohachi.study@gmail.com>
2024-02-17 18:08:10 +0900

    fix: user を json から取ってくるように変更

---
+++ js/sidebars/sidebar-user.js
@@ -0,0 +1,38 @@
+function generateUserDescriptions(jsonUrl, containerSelector, fetchCondition) {
+  fetch(jsonUrl)
+    .then((response) => response.json())
+    .then((users) => {
+      const container = document.querySelector(containerSelector);
+
+      users.forEach((user) => {
+        if (fetchCondition(user)) {
+          const userDiv = document.createElement("div");
+          userDiv.className = "user-description";
+
+          const img = document.createElement("img");
+          img.src = user.imgSrc;
+
+          const nameDiv = document.createElement("div");
+          nameDiv.id = `user-${user.id}`;
+          nameDiv.textContent = user.userName;
+
+          userDiv.appendChild(img);
+          userDiv.appendChild(nameDiv);
+
+          container.appendChild(userDiv);
+        }
+      });
+    });
+}
+
+generateUserDescriptions(
+  "../../data/sidebars/sidebar-user.json",
+  ".account-owner-description__container",
+  (user) => user.isMyself
+);
+
+generateUserDescriptions(
+  "../../data/sidebars/sidebar-user.json",
+  ".friends-description__container",
+  (user) => !user.isMyself
+);
