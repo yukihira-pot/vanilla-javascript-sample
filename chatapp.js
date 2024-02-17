239af5875c0e1902c1391d22e616ba39b762d76f
yukihira-pot <nagohachi.study@gmail.com>
2024-02-17 17:48:22 +0900

    add: talk, add-friend ページを追加

--- chatapp.js
+++ chatapp.js
@@ -1,8 +1,20 @@
 const icons = document.querySelectorAll(".navbar-icon__container i");
+const iframes = document.querySelectorAll("iframe");
 
 icons.forEach((icon) => {
   icon.addEventListener("click", function () {
     icons.forEach((icon) => icon.classList.remove("active"));
     this.classList.add("active");
+
+    const activeClass = Array.from(this.classList).find((className) =>
+      className.startsWith("attr-")
+    );
+
+    iframes.forEach((iframe) => (iframe.style.display = "none"));
+
+    const activeIframe = document.querySelector(`iframe.${activeClass}`);
+    if (activeIframe) {
+      activeIframe.style.display = "block";
+    }
   });
 });
